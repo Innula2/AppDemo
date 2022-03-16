f5a6c7c506949456543c8ae980478258c36753dc
Innula2 <91963982+Innula2@users.noreply.github.com>
2022-03-16 15:24:19 +0300

    Добавьте файлы проекта.

---
+++ AppDemo/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppDemo
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationContext()
+        {
+           // Database.EnsureDeleted();
+           // Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+           @"Server=(localdb)\mssqllocaldb;Database=efbasicsappdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ AppDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AppDemo
+{
+   
+    
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    
+                    Console.WriteLine("Объекты успешно сохранены");
+                    // получаем объекты из бд и выводим на консоль
+                    
+                }
+                Console.Read();
+            }
+        }
+    
+}
